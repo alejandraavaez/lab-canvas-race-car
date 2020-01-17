dea72256fb649ea69a9ea3ac4338817cf65e4e24
Karlish Vázquez <karlishvazquez@Karlishs-MacBook-Pro-748.local>
2020-01-16 19:13:52 -0600

    done

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,6 +1,7 @@
 let interval
 let frames = 0
 const obstaculos = []
+let score = 100
 
   const canvas = document.getElementById('race-car')
   const ctx = canvas.getContext('2d')
@@ -10,7 +11,7 @@ const obstaculos = []
       this.x = 0
       this.y = 0
       this.width = canvas.width
-      this.heigth = canvas.height
+      this.height = canvas.height
     }
     draw(){
       ctx.fillStyle = "green"
@@ -27,16 +28,14 @@ const obstaculos = []
       ctx.fillRect(370, 0, 10,800)
       ctx.fillStyle = "green"
       ctx.fillRect(380, 0, 20,800)
-      ctx.fillStyle = "white"
-      ctx.fillRect(200, 0, 5, 15)
-      ctx.fillStyle = "white"
-      ctx.fillRect(200, 20, 5, 15)
-      ctx.fillStyle = "white"
-      ctx.fillRect(200, 55, 5, 15)
-      ctx.fillStyle = "white"
-      ctx.fillRect(200, 85, 5, 15)
-      ctx.fillStyle = "white"
-      ctx.fillRect(200, 105, 5, 15)
+      ctx.fillStroke ="white"
+      ctx.beginPath()
+      ctx.lineWidth = 5
+      ctx.strokeStyle = "white"
+      ctx.setLineDash([15,15])
+      ctx.moveTo(200,0)
+      ctx.lineTo(200,800)
+      ctx.stroke()
     }
 
   }
@@ -45,21 +44,21 @@ const obstaculos = []
     constructor(x,y){
       this.x = x
       this.y = y
-      this.width = 400
-      this.height = 800
+      this.width = 40
+      this.height = 80
       this.sx = 180
       this.img = new Image()
       this.img.src = "./images/car.png"
     }
     drawCar(){
       // ctx.drawImage(this.img,200,700,40,60)
-      if( this.sx >= 400) this.sx = 0
+    
       ctx.drawImage(
         this.img,
-        this.sx, // es la posicion en x y se va a cambiar 
-        710,// es la posicion en y y es fija
-        40,// tamaño
-        80,
+        this.x, // es la posicion en x y se va a cambiar 
+        this.y,// es la posicion en y y es fija
+        this.width,// tamaño
+        this.height,
       )
     }
     goRigth(){
@@ -70,17 +69,27 @@ const obstaculos = []
       this.x -= 10
       this.sx -=10
     }
+    isTouching(obstacle){
+      return (
+        this.x < obstacle.x + obstacle.width &&
+        this.x + this.width > obstacle.x &&
+        this.y < obstacle.y + obstacle.height &&
+        this.y + this.height > obstacle.y
+      )
+    }
   }
 
   class obstacle{
-    constructor(x,y){
+    constructor(x){
       this.x = x
-      this.y = y
-      this.heigth = 20
+      this.y = 0
+      this.width = 50
+      this.height = 20
     }
     drawObstacle(){
+      this.y++
       ctx.fillStyle = "red"
-      ctx.fillRect(this.x,this.y,30,20)
+      ctx.fillRect(this.x,this.y,this.width,this.height)
     }
   }
  
@@ -115,16 +124,28 @@ window.onload = function() {
   function generateObstacles(){
     if(frames % 100 === 0){
       let rndX = Math.random() * (350 - 50) + 50
-      let rndY = Math.random() * 700
-      obstaculos.push(new obstacle(rndX, rndY))
+      obstaculos.push(new obstacle(rndX))
     }
+  }
+  function checkCollitions(){
+    
+    obstaculos.forEach((e, i) =>{
+      
+      console.log(player.isTouching(e));
+      
+      if(player.isTouching(e)){
+        return score -= 10
+      }
+    })
   }
 
   function update() {
     frames++
     ctx.clearRect(0,0, canvas.width, canvas.height)
     background.draw()
-    drawObstaculos()
     player.drawCar()
+    drawObstaculos()
+    checkCollitions()
+    ctx.fillText(String(score), canvas.width - 50, 50)
   }
 };
